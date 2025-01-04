4e0bfc95ac8e6a7ce5248e0d5f9c51e4ba0c71c2
ChristianCoons <christiananthonycoons@gmail.com>
2025-01-04 07:32:56 -0700

    C: rendering player triangle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,6 +8,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     running = True
     while(running):
@@ -15,7 +17,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        
         dt = clock.tick(60) / 1000
     
 
